7e04b27ef21d2f90fb952a5d18a2e78876e9ed80
12MaXX43 <newm33825@gmail.com>
2026-06-04 17:41:40 +0200

    everything adapted

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,25 @@
 const hamburger = document.querySelector(".hamburger")
 const header_menu = document.querySelector(".header_menu")
 
-hamburger.onclick = nav_toggle
-document.querySelectorAll('.header_menu a').forEach(function(a){
-    a.onclick = nav_toggle
-})
+if (window.matchMedia('(max-width: 664px)').matches) {
+    hamburger.onclick = nav_toggle
+    document.querySelectorAll('.header_menu a').forEach(function (a) {
+        a.onclick = nav_toggle
+    })
+    document.addEventListener('keydown', function (ev) {
+        if (ev.key === 'Escape') {
+            nav_close()
+        }
+    })
+}
 
-function nav_toggle(){
- hamburger.classList.toggle("header_hamburger")   
- header_menu.classList.toggle("menu_header")   
- document.body.classList.toggle("bodyblock")   
+function nav_toggle() {
+    hamburger.classList.toggle("header_hamburger")
+    header_menu.classList.toggle("menu_header")
+    document.body.classList.toggle("bodyblock")
+}
+function nav_close() {
+    hamburger.classList.remove("header_hamburger")
+    header_menu.classList.remove("menu_header")
+    document.body.classList.remove("bodyblock")
 }
